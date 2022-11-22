2a99dfd2fda3cbfc6a40d55128f9abaf60781595
Jose David Pérez Cañellas <jdtheripperpc@gmail.com>
2022-11-22 18:49:17 +0100

    Crear proyecto de ejemplo
    nuevo proyecto de netbeans

---
+++ Demo/src/demo/Demo.java
@@ -0,0 +1,16 @@
+package demo;
+
+/**
+ *
+ * @author JD The Ripper PC
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("#SomUIB");
+    }
+
+}
